a30a4f3c4095800c0fc29aef878c42e7bac6d058
Alejandro Juyo <ajuyog77@gmail.com>
2022-01-11 22:41:09 -0500

    Validación

---
+++ Inventory_1/Controllers/EnsambleController.cs
@@ -0,0 +1,23 @@
+﻿using Inventory_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_1.Controllers
+{
+    public class EnsambleController: Controller
+    {
+        public IActionResult CrearEnsamble()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearEnsamble(Ensamblajes ensamblajes)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ensamblajes);
+            }
+            return View();
+        }
+    }
+}
---
+++ Inventory_1/Models/Ensamblajes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_1.Models
+{
+    public class Ensamblajes
+    {
+        public int idAssambly { get; set; }
+        [Display(Name ="Elemento")]
+        [Range(minimum: 1,int.MaxValue, ErrorMessage ="El campo {0} es obligatorio")]
+        public int Element_idElement { get; set; }
+        [Display(Name ="Ensamble")]
+        [Range(minimum: 1, int.MaxValue, ErrorMessage = "El campo {0} es obligatorio")]
+        public int Assembly_idAssembly { get; set; }
+        [Display(Name ="No. Serial")]
+        public string serialnumber { get; set; }
+        [Display(Name ="Codigo Secundario")]
+        public string codSecondary { get; set; }
+    }
+}
