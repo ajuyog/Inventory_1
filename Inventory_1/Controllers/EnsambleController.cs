7beccf76da2ccf3808863a8957a1ae5f57f818e4
ajuyog <ajuyog77@gmail.com>
2022-01-12 21:02:16 -0500

    conexiones

--- Inventory_1/Controllers/EnsambleController.cs
+++ Inventory_1/Controllers/EnsambleController.cs
@@ -1,12 +1,24 @@
-﻿using Inventory_1.Models;
+﻿using Dapper;
+using Inventory_1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace Inventory_1.Controllers
 {
     public class EnsambleController: Controller
     {
+        private readonly string connectionString;
+        public EnsambleController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
         public IActionResult CrearEnsamble()
         {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
+
             return View();
         }
 
